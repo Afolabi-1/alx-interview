6dd7e337615883aa77649aa5f95542789f17aed9
afolabi <72478870+Afolabi-1@users.noreply.github.com>
2023-05-06 00:12:23 +0100

    Add files via upload

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+0. alx interview pascals triangle
+"""
+
+
+def pascal_triangle(n):
+    """Create a function that returns a list of lists
+    of integers representing the Pascal’s triangle of n
+    """
+    lis = []
+    if n > 0:
+        for i in range(1, n + 1):
+            els = []
+            C = 1
+            for j in range(1, i + 1):
+                els.append(C)
+                C = C * (i - j) // j
+            lis.append(els)
+    return lis
